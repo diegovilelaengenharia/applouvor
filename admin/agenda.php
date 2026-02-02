22f95d83071392e76297e6be042ee322d3997d34
Diego Vilela <diegonunesvilela@gmail.com>
2026-02-02 11:37:15 -0300

    Fix: Corrected placement of mobile detection logic in agenda.php

--- admin/agenda.php
+++ admin/agenda.php
@@ -5,7 +5,11 @@ require_once '../includes/db.php';
 require_once '../includes/layout.php';
 
 // Parâmetros de visualização
-$viewMode = $_GET['view'] ?? 'month'; // month, week, list
+// Detectar Mobile
+$isMobile = preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i', $_SERVER['HTTP_USER_AGENT'] ?? '');
+
+$defaultView = $isMobile ? 'list' : 'month';
+$viewMode = $_GET['view'] ?? $defaultView;
 // Normaliza view antiga 'calendar' para 'month'
 if ($viewMode === 'calendar') $viewMode = 'month';
 
@@ -371,16 +375,7 @@ renderPageHeader('Agenda', 'Calendário e eventos do ministério');
     }
 </style>
 
-<?php
-// Parâmetros de visualização
-// Detectar Mobile
-$isMobile = preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i', $_SERVER['HTTP_USER_AGENT'] ?? '');
 
-$defaultView = $isMobile ? 'list' : 'month';
-$viewMode = $_GET['view'] ?? $defaultView;
-// Normaliza view antiga 'calendar' para 'month'
-if ($viewMode === 'calendar') $viewMode = 'month';
-?>
 
 <div class="agenda-container">
     <div class="agenda-controls">
